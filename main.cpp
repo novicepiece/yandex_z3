97ab5ff3c9fa82d9229460ea599ad733e3fe81c4
Gavrilov Yaroslav <slava.gavrilov200@gmail.com>
2020-08-11 20:07:51 +0300

    added input

--- main.cpp
+++ main.cpp
@@ -1,9 +1,58 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <cstdlib>
 
-using namespace std;
+using std::cout;
+using std::cin;
+
+struct WorkCategory
+{
+    uint64_t N;
+    uint64_t P;
+};
+
+std::vector<WorkCategory> works;
+std::vector< std::vector<int> > applicants;
+
+void fileInput(const std::string& input_file = "input.txt")
+{
+    std::ifstream fin(input_file.c_str());
+
+    int n, m;
+    fin >> n >> m;
+
+    works.resize(n);
+    applicants.resize(m);
+
+    for (int i = 0; i < n; i++)
+    {
+        fin >> works[i].N;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        fin >> works[i].P;
+    }
+
+    for (int i = 0; i < m; i++)
+    {
+        int k;
+        fin >> k;
+        applicants[i].resize(k);
+        for (int j = 0; j < k; j++)
+        {
+            fin >> applicants[i][j];
+        }
+    }
+}
+
+
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    fileInput();
+
+
+
     return 0;
 }
